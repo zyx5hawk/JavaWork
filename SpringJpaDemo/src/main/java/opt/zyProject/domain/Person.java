06d6f19370335e3e834bc5e776f15a9718f6cb35
zyx <zyx_hawk@126.com>
2018-04-28 17:41:44 +0800

    add springJpaDemo

---
+++ SpringJpaDemo/src/main/java/opt/zyProject/domain/Person.java
@@ -0,0 +1,31 @@
+package opt.zyProject.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
+public class Person {
+	
+	private Integer id;
+	private String name;
+	
+	@Id
+	@GeneratedValue()
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	@Column(length = 12)
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	
+}
